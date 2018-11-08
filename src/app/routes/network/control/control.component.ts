598423c93c99f7ce0b7957a7d20ce07163665aff
李煜泽 <liyuze@liyuze.local>
2018-11-08 22:16:22 +0800

    约束控制页面 UI 基本完成

--- src/app/routes/network/control/control.component.ts
+++ src/app/routes/network/control/control.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 
 @Component({
   selector: 'app-network-control',
   templateUrl: './control.component.html',
+  styleUrls: ['./control.component.css']
 })
 export class NetworkControlComponent implements OnInit {
+
+  @Input() radioValue: string;
+
+  strategy_radioValue;
+  strategy_radioValue_all = 'strategy0';
+  strategy_internal = true;
+  strategy_internal_rate: any;
+  strategy_pollution = true;
+  strategy_pollution_rate: any;
 
   constructor(private http: _HttpClient) { }
 

0ed3b825495bcf2e5c527809e0982cb8a584a964
李煜泽 <409654850@qq.com>
2018-11-14 23:45:10 +0800

    约束控制页面 ts文件参数增加

--- src/app/routes/network/control/control.component.ts
+++ src/app/routes/network/control/control.component.ts
@@ -10,15 +10,30 @@ export class NetworkControlComponent implements OnInit {
 
   @Input() radioValue: string;
 
-  strategy_radioValue;
-  strategy_radioValue_all = 'strategy0';
+  // strategy_radioValue;
+  // strategy_radioValue_all = 'strategy0';
   strategy_internal = true;
   strategy_internal_rate: any;
   strategy_pollution = true;
   strategy_pollution_rate: any;
 
+  form_data =  {
+    system : {
+      strategy_radioValue : 'strategy1',
+      strategy_radioValue_all : 'strategy0',
+      strategy_internal_rate : '0.00',
+      strategy_pollution_rate : '0.00',
+      buy_constraint_1 : '0.00',
+      sell_constraint_2 : '0.00'
+    },
+  };
+
   constructor(private http: _HttpClient) { }
 
   ngOnInit() { }
+
+  getData() {
+    console.log(this.form_data);
+  }
 
 }
